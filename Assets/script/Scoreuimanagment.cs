38be41a16ffd2c278fd11eee86ae6a4b9054828a
49960011-hue <49960011@est.ort.edu.ar>
2026-04-29 14:31:28 -0300

    a

---
+++ Assets/script/Scoreuimanagment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreuimanagment : MonoBehaviour
+{ public textMeshProUGUI txtScore;
+        public int score =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+       txt_score.text=score.ToString();
+    }
+
+    // Update is called once per frame
+    void addpointAndUpdateScore()
+    {
+        score ++;
+        Txt_score.text=score.ToString();
+    }
+}
