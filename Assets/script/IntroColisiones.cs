e7bd6b980f360b4cd4526eb29840a1d905175205
49960011-hue <49960011@est.ort.edu.ar>
2026-04-29 09:02:29 -0300

    Initial commit

---
+++ Assets/script/IntroColisiones.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroColisiones : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision col)
+    {
+
+        Debug.Log("Hubo una colision"+col.gameObject.name);
+    }
+}
